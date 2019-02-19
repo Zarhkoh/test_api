7e3271fc80eb1f54776be3e1ec3848318b662648
Zarhkoh <sullivan.delaby@gmail.com>
2019-02-19 14:07:58 +0100

    fix bug with composer versions (db migration included)

--- src/Migrations/Version20190219111243.php
+++ src/Migrations/Version20190219111243.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190214111718 extends AbstractMigration
+final class Version20190219111243 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,9 +22,6 @@ final class Version20190214111718 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE student (id INT AUTO_INCREMENT NOT NULL, firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, response TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE resource (id INT AUTO_INCREMENT NOT NULL, soft VARCHAR(255) DEFAULT NULL, alcohol VARCHAR(255) DEFAULT NULL, food VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE resource_student (resource_id INT NOT NULL, student_id INT NOT NULL, INDEX IDX_EA8348DB89329D25 (resource_id), INDEX IDX_EA8348DBCB944F1A (student_id), PRIMARY KEY(resource_id, student_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE resource_student ADD CONSTRAINT FK_EA8348DB89329D25 FOREIGN KEY (resource_id) REFERENCES resource (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE resource_student ADD CONSTRAINT FK_EA8348DBCB944F1A FOREIGN KEY (student_id) REFERENCES student (id) ON DELETE CASCADE');
     }
@@ -34,10 +31,7 @@ final class Version20190214111718 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE resource_student DROP FOREIGN KEY FK_EA8348DBCB944F1A');
         $this->addSql('ALTER TABLE resource_student DROP FOREIGN KEY FK_EA8348DB89329D25');
-        $this->addSql('DROP TABLE student');
-        $this->addSql('DROP TABLE resource');
-        $this->addSql('DROP TABLE resource_student');
+        $this->addSql('ALTER TABLE resource_student DROP FOREIGN KEY FK_EA8348DBCB944F1A');
     }
 }
